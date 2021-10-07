301798657df281decb85c1096d4ec017317cba99
Abhishek Raj <abhi@raj.me>
2021-10-07 19:43:19 +0530

    Remove test folder

--- test/01/src/index.js
+++
@@ -1,6 +0,0 @@
-const dapi = require('starter-lib');
-
-console.log('>>', dapi);
-console.log('>>', dapi.default);
-console.log('>>', dapi.foo);
-console.log('>>', dapi.num);
--- test/02/src/index.mjs
+++
@@ -1,5 +0,0 @@
-import dapi, { foo, num } from 'starter-lib';
-
-console.log('>>', dapi);
-console.log('>>', foo);
-console.log('>>', num);
