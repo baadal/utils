fd7618a67fde709e25dbc9cfe495c8fa4c222305
Abhishek Raj <abhi@raj.me>
2021-10-10 13:37:12 +0530

    Add util: arrayToMap

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -31,3 +31,24 @@ export const base64Decode = (base64: string) => {
 export const sha1Hash = (data: string) => {
   return sha1(data) as string;
 };
+
+/**
+ * Create a Map from a given Array based on some field
+ * Note that the items of the array should be of type `object`
+ * @param arr given Array to be converted to Map
+ * @param key a string field from Array items
+ * @returns a map created from the array items
+ */
+export const arrayToMap = <T = any>(arr: T[] | null, key: string) => {
+  if (!arr || !Array.isArray(arr) || !arr.length) return null;
+  const map = new Map<string, T>();
+  arr.forEach((item: any) => {
+    if (typeof item === 'object') {
+      const itemKey = item[key];
+      if (itemKey && typeof itemKey === 'string') {
+        map.set(itemKey, item);
+      }
+    }
+  });
+  return map.size ? map : null;
+};
